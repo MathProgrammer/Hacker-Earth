cffd2be7c32695e328d9f6490824f55a651d99db
Saikat Ghosh <ghosh_saikat4000@yahoo.co.in>
2017-10-26 19:39:42 +0530

    Add files via upload

---
+++ C Programs/C Programs - 3/Game of Coins.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+
+int main()
+{
+    int no_of_test_cases;
+    scanf("%d", &no_of_test_cases);
+
+    while(no_of_test_cases--)
+    {
+        int no_of_coins;
+        scanf("%d", &no_of_coins);
+
+        printf("Alice\n");
+    }
+
+    return 0;
+}

d110e162bc212ab0aac301acca98c28b4a85af69
Saikat Ghosh <ghosh_saikat4000@yahoo.co.in>
2018-02-11 16:02:53 +0530

    Update Points on a Rectangle.cpp

--- C Programs/C Programs - 4/Points on a Rectangle.cpp
+++ C Programs/C Programs - 4/Points on a Rectangle.cpp
@@ -5,27 +5,27 @@
 
 void solve()
 {
-    int x1, y1, x2, y2, n, no_of_points = 0;
+    int x1, y1, x2, y2, no_of_points, no_of_internal_points = 0;
     scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
 
-    scanf("%d", &n);
+    scanf("%d", &no_of_points);
 
-    while(n--)
+    while(no_of_points--)
     {
         int x, y;
         scanf("%d %d", &x, &y);
 
-        no_of_points += (x > min(x1, x2) && x < max(x1, x2) && y > min(y1, y2) && y<max(y1, y2));
+        no_of_internal_points += (x > min(x1, x2) && x < max(x1, x2) && y > min(y1, y2) && y<max(y1, y2));
     }
-    printf("%d\n", no_of_points);
+    printf("%d\n", no_of_internal_points);
 }
 
 int main()
 {
-    int t;
-    scanf("%d", &t);
+    int no_of_test_cases;
+    scanf("%d", &no_of_test_cases);
 
-    while(t--)
+    while(no_of_test_cases--)
         solve();
 
     return 0;

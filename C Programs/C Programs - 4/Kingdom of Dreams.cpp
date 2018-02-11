1f67b0de547cd661b21999235fbea11b13ce43e3
Saikat Ghosh <ghosh_saikat4000@yahoo.co.in>
2018-02-11 21:08:13 +0530

    Update Kingdom of Dreams.cpp

--- C Programs/C Programs - 4/Kingdom of Dreams.cpp
+++ C Programs/C Programs - 4/Kingdom of Dreams.cpp
@@ -9,29 +9,27 @@ void solve()
     int no_of_people;
     scanf("%d", &no_of_people);
 
-    vector <int> cost(no_of_people);
+    vector <int> cost(no_of_people + 1, 0);
     for(int i = 0; i < no_of_people; i++)
         scanf("%d", &cost[i]);
-
-    no_of_people--;
 
     sort(all(cost));
 
     long long total_cost = 0;
 
-    while(no_of_people >= 3)
+    while(no_of_people >= 4)
     {
-        total_cost += min(cost[1] + cost[0] + cost[no_of_people] + cost[1],
-                          cost[no_of_people] + cost[0] + cost[no_of_people - 1] + cost[0]);
+        total_cost += min(cost[2] + cost[1] + cost[no_of_people] + cost[2],
+                          cost[no_of_people] + cost[1] + cost[no_of_people - 1] + cost[1]);
 
         no_of_people -= 2;
     }
 
-    if(no_of_people == 2) total_cost += cost[2] + cost[0] + cost[1];
+    if(no_of_people == 3) total_cost += cost[3] + cost[1] + cost[2];
+
+    if(no_of_people == 2) total_cost += cost[2];
 
     if(no_of_people == 1) total_cost += cost[1];
-
-    if(no_of_people == 0) total_cost += cost[0];
 
     printf("%lld\n", total_cost);
 }

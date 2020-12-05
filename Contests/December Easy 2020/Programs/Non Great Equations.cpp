81defa8b10a3879a4834fb0065b443945798509b
Saikat Ghosh <ghosh_saikat4000@yahoo.co.in>
2020-12-05 12:53:35 +0530

    Create Non Great Equations.cpp

---
+++ Contests/December Easy 2020/Programs/Non Great Equations.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	long long a, b, c;
+	cin >> b;
+
+	c = (b*b + b)/2;
+	a = (b*b - b)/2;
+
+	cout << a << " " << c << "\n";
+	return 0;
+}
